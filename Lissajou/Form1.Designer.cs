a3129a0a9266158cc7dd88b3b4e32e4a8658c39c
poplawski_r <poplawski@tecna.pl>
2016-06-21 08:38:44 +0200

    Init

---
+++ Lissajou/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btDraw = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btSave = new System.Windows.Forms.Button();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btDraw
+            // 
+            this.btDraw.Location = new System.Drawing.Point(12, 231);
+            this.btDraw.Name = "btDraw";
+            this.btDraw.Size = new System.Drawing.Size(75, 23);
+            this.btDraw.TabIndex = 0;
+            this.btDraw.Text = "Draw";
+            this.btDraw.UseVisualStyleBackColor = true;
+            this.btDraw.Click += new System.EventHandler(this.btDraw_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 2;
+            this.hScrollBar1.Location = new System.Drawing.Point(1, 206);
+            this.hScrollBar1.Maximum = 10;
+            this.hScrollBar1.Minimum = 1;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(201, 17);
+            this.hScrollBar1.TabIndex = 1;
+            this.hScrollBar1.Value = 1;
+            this.hScrollBar1.ValueChanged += new System.EventHandler(this.hScrollBar1_ValueChanged);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.LargeChange = 3;
+            this.vScrollBar1.Location = new System.Drawing.Point(205, 2);
+            this.vScrollBar1.Maximum = 10;
+            this.vScrollBar1.Minimum = 1;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 201);
+            this.vScrollBar1.TabIndex = 2;
+            this.vScrollBar1.Value = 1;
+            this.vScrollBar1.ValueChanged += new System.EventHandler(this.vScrollBar1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 201);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(178, 231);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 4;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Location = new System.Drawing.Point(239, 2);
+            this.vScrollBar2.Maximum = 180;
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(17, 201);
+            this.vScrollBar2.TabIndex = 5;
+            this.vScrollBar2.ValueChanged += new System.EventHandler(this.vScrollBar2_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 233);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(79, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Pict_";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 259);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.btDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btDraw;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lissajou/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        private void doDrawing()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            Graphics gr = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            gr.Clear(Color.Beige);
+
+            double x, y, x0, y0;
+            double kx, ky;
+            kx = hScrollBar1.Value + 1;
+            ky = vScrollBar1.Value + 1;
+            x0 = 0;
+            y0 = 0;
+            Color c;
+            int n = (int)(kx * ky);
+            for (int t = 0; t < 360 * n; t += 1)
+            {
+                int r, g, b;
+                r = 0; g = 0; b = 0;
+                int d = vScrollBar2.Value;
+                r = (int)(128 + 127 * Math.Pow(0.5 + 0.5 * Math.Sin((t / n -   0.0 - d) / 90.0 * Math.PI), 4));
+                g = (int)(128 + 127 * Math.Pow(0.5 + 0.5 * Math.Sin((t / n - 120.0 - d) / 90.0 * Math.PI), 4));
+                b = (int)(128 + 127 * Math.Pow(0.5 + 0.5 * Math.Sin((t / n - 240.0 - d) / 90.0 * Math.PI), 4));
+
+                c = Color.FromArgb(r, g, b);
+                Pen pn = new Pen(c, 15); 
+                x = 100 + 100 * Math.Cos((t - 0) / 180.0 * Math.PI / kx);
+                y = 100 + 100 * Math.Sin((t - ky * d) / 180.0 * Math.PI / ky);
+                if (t != 0) 
+                   gr.DrawLine(pn, (float)x0, (float)y0, (float)x, (float)y);
+                x0 = x;
+                y0 = y;
+            }
+        }
+
+        private void btDraw_Click(object sender, EventArgs e)
+        {
+            doDrawing();
+        }
+
+        private void vScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            doDrawing();
+        }
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            doDrawing();
+        }
+
+        private void vScrollBar2_ValueChanged(object sender, EventArgs e)
+        {
+            doDrawing();
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+            string fname = textBox1.Text + string.Format("{0:03}", vScrollBar2.Value) + ".png";
+            pictureBox1.Image.Save(fname, System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
